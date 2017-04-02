5bdbf792ebcdedd3659362a551d49430336014ed
patrick-9876 <patrick-9876@users.noreply.github.com>
2017-04-02 11:51:08 +0200

    Update NoteFactory.js

--- public_html/factories/NoteFactory.js
+++ public_html/factories/NoteFactory.js
@@ -4,11 +4,11 @@ app.factory('NoteFactory', function () {
         var Notes = localItems;
     } else {
         var Notes = [
-            {id: 1, title: "Note 1", tekst: "", notebook: 1, user: 1},
-            {id: 2, title: "Note 2", tekst: "", notebook: 1, user: 1},
-            {id: 3, title: "Note 3", tekst: "", notebook: 2, user: 1},
-            {id: 4, title: "Note 4", tekst: "", notebook: 2, user: 1},
-            {id: 5, title: "Note 5", tekst: "", notebook: 1, user: 1}
+            {id: 0, title: "Note 1", tekst: "", notebook: 1, user: 1},
+            {id: 1, title: "Note 2", tekst: "", notebook: 1, user: 1},
+            {id: 2, title: "Note 3", tekst: "", notebook: 2, user: 1},
+            {id: 3, title: "Note 4", tekst: "", notebook: 2, user: 1},
+            {id: 4, title: "Note 5", tekst: "", notebook: 1, user: 1}
         ];
         localStorage.setItem("Notes", angular.toJson(Notes));
     }
@@ -28,7 +28,7 @@ app.factory('NoteFactory', function () {
     }
 
     function createNote(NoteTitle, NoteTekst, NotebookID, UserID) {
-        Notes.push({id: Notes.length + 1, title: NoteTitle, tekst: NoteTekst, notebook: NotebookID, user: UserID});
+        Notes.push({id: Notes.length, title: NoteTitle, tekst: NoteTekst, notebook: NotebookID, user: UserID});
 
         localStorage.setItem('Notes', angular.toJson(Notes));
     }
